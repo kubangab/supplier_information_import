4a4e6bc77bff6f647c13e7bfc93f95900edfdf28
kubangab <lasse.larsson@kubang.eu>
2024-07-24 21:47:41 +0200

    First version to send email
    This version will add the ability to send email from orders with information of what is delivered and all necessary information.

--- models/stock_picking.py
+++ models/stock_picking.py
@@ -1,4 +1,7 @@
 from odoo import models, fields, api, _
+import base64
+import xlsxwriter
+from io import BytesIO
 from odoo.exceptions import UserError
 
 class StockPicking(models.Model):
@@ -50,3 +53,58 @@ class StockPicking(models.Model):
             picking.action_assign()
 
         return True
+    
+    def action_generate_and_send_excel(self):
+        self.ensure_one()
+        if self.state != 'done':
+            raise UserError(_("You can only generate the Excel file for confirmed transfers."))
+
+        excel_data = self.generate_excel_report()
+        self.send_excel_report_email(excel_data)
+
+    def generate_excel_report(self):
+        output = BytesIO()
+        workbook = xlsxwriter.Workbook(output)
+        worksheet = workbook.add_worksheet('Product Info')
+
+        # Add headers
+        headers = ['Product', 'Quantity', 'Serial Number', 'Supplier', 'Supplier Product Code', 'MAC1', 'MAC2', 'IMEI', 'AppKey', 'DevEUI']
+        for col, header in enumerate(headers):
+            worksheet.write(0, col, header)
+
+        # Add product data
+        for row, move_line in enumerate(self.move_line_ids, start=1):
+            product = move_line.product_id
+            incoming_info = self.env['incoming.product.info'].search([
+                ('product_id', '=', product.id),
+                ('sn', '=', move_line.lot_id.name)
+            ], limit=1)
+
+            worksheet.write(row, 0, product.name)
+            worksheet.write(row, 1, move_line.qty_done)
+            worksheet.write(row, 2, move_line.lot_id.name if move_line.lot_id else '')
+            worksheet.write(row, 3, self.partner_id.name)
+            worksheet.write(row, 4, incoming_info.supplier_product_code if incoming_info else '')
+            worksheet.write(row, 5, incoming_info.mac1 if incoming_info else '')
+            worksheet.write(row, 6, incoming_info.mac2 if incoming_info else '')
+            worksheet.write(row, 7, incoming_info.imei if incoming_info else '')
+            worksheet.write(row, 8, incoming_info.app_key if incoming_info else '')
+            worksheet.write(row, 9, incoming_info.dev_eui if incoming_info else '')
+
+        workbook.close()
+        return output.getvalue()
+
+    def send_excel_report_email(self, excel_data):
+        attachment = self.env['ir.attachment'].create({
+            'name': f'Product_Info_{self.name}.xlsx',
+            'datas': base64.b64encode(excel_data),
+            'res_model': 'stock.picking',
+            'res_id': self.id,
+        })
+
+        template = self.env.ref('supplier_information_import.email_template_product_info')
+        template.send_mail(
+            self.id,
+            force_send=True,
+            email_values={'attachment_ids': [attachment.id]}
+        )
