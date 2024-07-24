9a9207f96361a60d73220b7827242485c3739f37
kubangab <lasse.larsson@kubang.eu>
2024-07-24 22:00:09 +0200

    Corrected bug for email

--- models/stock_picking.py
+++ models/stock_picking.py
@@ -102,7 +102,7 @@ class StockPicking(models.Model):
             'res_id': self.id,
         })
 
-        template = self.env.ref('supplier_information_import.email_template_product_info')
+        template = self.env.ref('product_information_import.email_template_product_info')
         template.send_mail(
             self.id,
             force_send=True,
