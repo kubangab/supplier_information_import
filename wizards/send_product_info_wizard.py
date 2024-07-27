d33352149a13608ea729aa64ebb4e2e82175c857
kubangab <lasse.larsson@kubang.eu>
2024-07-27 14:57:03 +0200

    Update send_product_info_wizard.py

--- wizards/send_product_info_wizard.py
+++ wizards/send_product_info_wizard.py
@@ -14,25 +14,44 @@ class SendProductInfoWizard(models.TransientModel):
         res = super(SendProductInfoWizard, self).default_get(fields)
         active_model = self._context.get('active_model')
         active_id = self._context.get('active_id')
-    
+
         if active_model and active_id:
-            record = self.env[active_model].browse(active_id)
-            template = self.env.ref('supplier_information_import.email_template_product_info')
-    
-            # Generate Excel file
-            excel_data = record.generate_excel_report()
-            attachment = self.env['ir.attachment'].create({
-                'name': f'Product_Info_{record.name}.xlsx',
-                'datas': excel_data,
-                'res_model': active_model,
-                'res_id': active_id,
-            })
-    
-            res.update({
-                'subject': template._render_field('subject', [record.id])[record.id],
-                'body': template._render_field('body_html', [record.id])[record.id],
-                'attachment_id': attachment.id,
-            })
+            try:
+                record = self.env[active_model].sudo().browse(active_id).exists()
+                if not record:
+                    raise UserError(_("The record no longer exists. Please refresh your browser."))
+
+                template = self.env.ref('supplier_information_import.email_template_product_info')
+
+                # Generate Excel file
+                excel_data = record.generate_excel_report()
+                attachment = self.env['ir.attachment'].sudo().create({
+                    'name': f'Product_Info_{record.name}.xlsx',
+                    'datas': base64.b64encode(excel_data),
+                    'res_model': active_model,
+                    'res_id': active_id,
+                })
+
+                # Render e-mail template
+                template_ctx = {
+                    'object': record,
+                }
+                subject = self.env['mail.render.mixin'].with_context(**template_ctx)._render_template(
+                    template.subject, active_model, [active_id])[active_id]
+                body = self.env['mail.render.mixin'].with_context(**template_ctx)._render_template(
+                    template.body_html, active_model, [active_id])[active_id]
+
+                res.update({
+                    'subject': subject,
+                    'body': body,
+                    'attachment_id': attachment.id,
+                })
+            except UserError as ue:
+                raise ue
+            except Exception as e:
+                _logger.error(f"Error in generating product info: {str(e)}")
+                raise UserError(_("An error occurred while preparing the email. Please try again or contact your administrator."))
+
         return res
 
     def action_send_mail(self):
