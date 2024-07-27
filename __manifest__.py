cc53d038755feda54b59212516644156e1fa13f6
kubangab <lasse.larsson@kubang.eu>
2024-07-27 22:12:35 +0200

    Coorected error

--- __manifest__.py
+++ __manifest__.py
@@ -1,7 +1,7 @@
 #  © opyright 2024 Lasse Larsson, Kubang AB
 {
     'name': 'Supplier Information Import',
-    'version': '16.0.4.0.1',
+    'version': '16.0.4.1.0',
     'category': 'Inventory',
     'summary': 'Import and manage incoming product information',
     'description': """
--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,5 @@
+from . import mixin
 from . import incoming_product_info
 from . import import_config
 from . import stock_picking
 from . import sale_order
-from . import mixin
