6fce78a9922ae0ab2c41c84ad9de1d5f294a5829
kubangab <lasse.larsson@kubang.eu>
2024-08-29 13:11:18 +0200

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -1,7 +1,7 @@
 #  © opyright 2024 Lasse Larsson, Kubang AB
 {
     'name': 'Supplier Information Import',
-    'version': '16.4.0.0.0',
+    'version': '16.4.0.0.1',
     'category': 'Inventory',
     'summary': 'Import and manage incoming product information',
     'description': """
